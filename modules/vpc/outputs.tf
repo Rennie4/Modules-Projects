1aa3c64ff82b34c401a0c486fb8a7e5df7834bb4
Rennie4 <rgovender2@oldmutual.com>
2025-07-24 12:42:59 +0000

    updated subnet names

--- modules/vpc/outputs.tf
+++ modules/vpc/outputs.tf
@@ -2,11 +2,11 @@ output "vpc_id" {
   value = aws_vpc.main.id
 }
 
-output "private-subnet1_id" {
+output "private_subnet1_id" {
   value = aws_subnet.private_subnet1.id
 }
 
-output "private-subnet2_id" {
+output "private_subnet2_id" {
   value = aws_subnet.private_subnet2.id
 }
 

f2900920c55cac0fd10c3d6737672aaa73c2945a
Rennie4 <rgovender2@oldmutual.com>
2025-07-24 12:23:57 +0000

    added vulues to module vpc

--- main.tf
+++ main.tf
@@ -1,4 +1,10 @@
 module "vpc" {
   source = "./modules/vpc" # Changed from "modules/vpc"
-  # ... other configurations ...
+  region                           = "eu-west-1"
+vpc_cidr                         = "10.0.0.0/16"
+private_subnet1_cidr             = "10.0.1.0/24"
+private_subnet2_cidr             = "10.0.2.0/24"
+transit_gateway_destination_cidr = "10.100.0.0/16" # Example: another VPC's CIDR
+
+  
 }

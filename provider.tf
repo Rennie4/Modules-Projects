75d7e4288d5bae64c78f0953b84e232cfb180666
Rennie4 <rgovender2@oldmutual.com>
2025-07-24 11:59:25 +0000

    updated provider file

--- provider.tf
+++ provider.tf
@@ -1,5 +1,19 @@
 provider "aws" {
-  region = "eu-west-1""
+  region = "eu-west-1"
+}
+data "aws_caller_identity" "current" {}
+output "caller" {
+  value = data.aws_caller_identity.current
 }
 
+
+
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
 
